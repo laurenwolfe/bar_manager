fe9155e7a0b58e094294074e812400b45093b145
laurenwolfe <wolfela@uw.edu>
2019-03-14 14:47:15 -0700

    autocomplete in progress. can currently select an element from input field and it will be displayed in list below.

--- app.js
+++ app.js
@@ -3,30 +3,43 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var hbs = require('hbs');
 
 /* ROUTER INCLUDES */
-
 var indexRouter = require('./routes/index');
 var cocktailRouter = require('./routes/cocktail');
 var cocktailResRouter = require('./routes/cocktail_results');
 
-
 var app = express();
 
-// view engine setup
+/* VIEW ENGINE SETUP */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+var blocks = {};
+
+hbs.registerHelper('extend', function(name, context) {
+  var block = blocks[name];
+
+  if (!block) {
+    block = blocks[name] = [];
+  }
+
+  block.push(context.fn(this));
+});
+
+hbs.registerHelper('block', function(name) {
+  var val = (blocks[name] || []).join('\n');
+  blocks[name] = [];
+  return val;
+});
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 /* ROUTER INSTANTIATION */
-
 app.use('/', indexRouter);
 app.use('/cocktail', cocktailRouter);
 app.use('/cocktail_results', cocktailResRouter);
@@ -38,7 +51,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/cocktail_results.js
+++ routes/cocktail_results.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
    res.render('cocktail_results', {
       results: [
          { _id: 1, name: "20th Century" },
